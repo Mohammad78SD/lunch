d4c3ab0a0f2b5406d7196857fc1277cda3480b28
Mohammad78SD <mohammadsd2000@gmail.com>
2024-10-19 20:46:04 +0330

    Refactor attendance URLs and add new views for changing and requesting attendance

--- MetafanLunch/attendance/urls.py
+++ MetafanLunch/attendance/urls.py
@@ -1,7 +1,12 @@
 from django.urls import include, path
-from .views import attendance, attendance_list
+from .views import attendance, attendance_list, change_attendance, user_requests
 
 urlpatterns = [
     path('', attendance, name='attendance'),
     path('list/', attendance_list, name='attendance_list'),
+    path('change/<int:attendance_id>/', change_attendance, name='change_attendance'),
+    path('request/', change_attendance, name='request_attendance'),
+    path('user-requests/', user_requests, name='user_requests'),
+
+
 ]

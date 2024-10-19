78f3a1ea618c359ce5581e01e71d3607d75899c8
Mohammad78SD <mohammadsd2000@gmail.com>
2024-10-19 20:45:47 +0330

    Add admin configuration for AttendanceRequest model

--- MetafanLunch/attendance/admin.py
+++ MetafanLunch/attendance/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
-from .models import AttendaceRecord
+from .models import AttendaceRecord, AttendanceRequest
 # Register your models here.
 admin.site.register(AttendaceRecord)
+
+
+class AttendanceRequestAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date', 'status', 'created_at')
+    list_filter = ('status', 'created_at')
+    search_fields = ('user__first_name', 'user__last_name', 'date')
+    date_hierarchy = 'created_at'
+    ordering = ('created_at',)
+admin.site.register(AttendanceRequest, AttendanceRequestAdmin)

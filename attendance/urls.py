f2c1a700e170ea271aaf7d7984797016da433c88
Mohammad78SD <mohammadsd2000@gmail.com>
2025-03-30 15:33:39 +0330

    Refactor attendance URL patterns for improved organization and readability

--- attendance/urls.py
+++ attendance/urls.py
@@ -1,14 +1,19 @@
 from django.urls import include, path
-from .views import attendance, attendance_list, change_attendance, user_requests, report_attendance, attendance_api
+from .views import (
+    attendance,
+    attendance_list,
+    change_attendance,
+    user_requests,
+    report_attendance,
+    attendance_api,
+)
 
 urlpatterns = [
-    path('', attendance, name='attendance'),
-    path('list/', attendance_list, name='attendance_list'),
-    path('change/<int:attendance_id>/', change_attendance, name='change_attendance'),
-    path('request/', change_attendance, name='request_attendance'),
-    path('user-requests/', user_requests, name='user_requests'),
-    path('download-attendance-report/', report_attendance, name='report_attendance'),
+    # path('', attendance, name='attendance'),
+    path("list/", attendance_list, name="attendance_list"),
+    path("change/<int:attendance_id>/", change_attendance, name="change_attendance"),
+    path("request/", change_attendance, name="request_attendance"),
+    path("user-requests/", user_requests, name="user_requests"),
+    path("download-attendance-report/", report_attendance, name="report_attendance"),
     path("api/", attendance_api, name="attendance_api"),
-
-
 ]

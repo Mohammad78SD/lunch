187f2884bb442b69f7b33bcee729fdf0db1f10f7
Mohammad78SD <mohammadsd2000@gmail.com>
2025-05-27 09:07:21 +0330

    Format URLs in lunch app for consistency and readability

--- lunch/urls.py
+++ lunch/urls.py
@@ -2,12 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('register/', views.register, name='register'),
-    path('login/', views.login_view, name='login'),
-    path('otp-verify/<str:phone_number>/', views.otp_verify_view, name='otp_verify'),
-    path('reserve_lunch/', views.reserve_lunch, name='reserve_lunch'),
-    path('log-out/', views.logout_view, name='account_logout'),
-    path('profile/', views.profile_info, name='profile_info'),
-    path('send_lunch_reservations_sms/', views.send_lunch_reservation_sms, name='send_lunch_reservations_sms'),
-    
+    path("register/", views.register, name="register"),
+    path("login/", views.login_view, name="login"),
+    path("otp-verify/<str:phone_number>/", views.otp_verify_view, name="otp_verify"),
+    path("reserve_lunch/", views.reserve_lunch, name="reserve_lunch"),
+    path("log-out/", views.logout_view, name="account_logout"),
+    path("profile/", views.profile_info, name="profile_info"),
+    path(
+        "send_lunch_reservations_sms/",
+        views.send_lunch_reservation_sms,
+        name="send_lunch_reservations_sms",
+    ),
+    path("form/", views.create_working_form, name="form_view"),
 ]
